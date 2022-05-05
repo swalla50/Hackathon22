f75ddbe3542b0bf0166a8d50ff4a43143d74147b
swalla50 <stevenwallacejr@aol.com>
2022-05-04 23:42:12 -0400

    Saving the selected options using change method

--- src/app/Components/edit-user/edit-user.component.ts
+++ src/app/Components/edit-user/edit-user.component.ts
@@ -13,25 +13,14 @@ export class EditUserComponent implements OnInit {
   hierarchy: any;
   optiontype: any;
 
-  OptionType = [
-    { id: 1, name: "" },
-    { id: 2, name: "Contingency Option Reminder" },
-    { id: 3, name: "Contraction Option Reminder" },
-    { id: 4, name: "Expansion Option Reminder" },
-    { id: 5, name: "Lease Expiration" },
-    { id: 6, name: "Other Option Reminder"},
-    { id: 7, name: "Other Option Reminder"},
-    { id: 8, name: "Purchase Option Reminder"},
-    { id: 9, name: "Relocation Option Reminder"},
-    { id: 10, name: "Automatic Renewal Option Reminder"}
-  ];
+ 
 
   Hierarchy:any = (i :any,n: any) => [
     { id: i, name: n }
   ]
 
-  selectedOptionType = [{ id: null, name: null}];
-  selectedHierarchy = [{ id: null, name: null}];
+  selectedOptionType: any = [];
+  selectedHierarchy:any = [];
 
   
   constructor( public service:SharedService) { }
@@ -40,13 +29,31 @@ export class EditUserComponent implements OnInit {
 
     this.service.getHierarchy().subscribe(data =>{
       this.hierarchy = data;
+
+      console.log("Hierarchy:", this.hierarchy)
     })
 
     this.service.getOptionType().subscribe(data =>{
       this.optiontype = data;
+
+      console.log("OptionType:", this.optiontype)
     })
     
   }
+
+
+  changeH(val:any) {
+    console.log("Hierarchy selection:", val);
+  }
+  changeOT(val:any) {
+    console.log("OptionType selection:", val);
+  }
+  
+
+
+
+
+
 
   
 
